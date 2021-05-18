2856ac08f399dd60466857ef9053e3995ec49a57
Christophe Driant <driantchristophe@gmail.com>
2021-05-18 16:14:26 +0200

    Remplacement du ViewBag par un ViewModel pour affichage des Articles

---
+++ ViewsModel/BlogViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.ViewsModel
+{
+    public class BlogViewModel
+    {
+        private IList<Article> article;
+
+        public IList<Article> Articles
+        {
+            get { return article; }
+            set { article = value; }
+        }
+
+
+    }
+}
